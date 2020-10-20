32107ed7405455516175760df6aac4195d82ec35
srue1834 <53914141+srue1834@users.noreply.github.com>
2020-10-19 19:02:26 -0600

    Update CipherProject.cpp

--- CipherProject.cpp
+++ CipherProject.cpp
@@ -16,6 +16,7 @@ Fecha de creación: 04/10/2020
 **********
 
 Proyecto 2: Cifrado y Descifrado de Textos DES
+Referencia para implementacion de DES: https://www.educative.io/edpresso/how-to-implement-the-des-algorithm-in-cpp 
 
 **********
 */
@@ -46,11 +47,11 @@ string const outputFileName = "salida.txt";													//Nombre del archivo de 
 string keyWord = "0100010000110011011001100100000001010101010011000101010001011111";		//Palabra binaria clave de cifrado predeterminada(default)	(editable)
 string cypherDone = "";																		//Variable que almacenara el texto cifrado					(no editable)
 string decypherDone = "";																	//Variable que almacenara el texto descifrado				(no editable)
-int const bufferLength = 8;																	//Espacio del buffer (cuantas letras se cifran seguidas)	(¡¡¡peligroso editar!!!)
+int const bufferLength = 4;																	//Espacio del buffer (cuantas letras se cifran seguidas)	(¡¡¡peligroso editar!!!)
 int const threadCount = 4;																	//Numero de threads utilizados								(editable)
 string round_keys[2];																		//Llaves totales generadas									(no editable)
 bool firstDone = false;
-
+/*
 int initialPemutation[64] = {58, 50, 42, 34, 26, 18, 10, 2,
 							 60, 52, 44, 36, 28, 20, 12, 4,
 							 62, 54, 46, 38, 30, 22, 14, 6,
@@ -268,106 +269,97 @@ void *cypherText(void *argument){
 	oldString = asciiToBinary(oldString);
 	
 	// The initial permutation table 
-    int initial_permutation[64] = { 
-    58,50,42,34,26,18,10,2, 
-    60,52,44,36,28,20,12,4, 
-    62,54,46,38,30,22,14,6, 
-    64,56,48,40,32,24,16,8, 
-    57,49,41,33,25,17,9,1, 
-    59,51,43,35,27,19,11,3, 
-    61,53,45,37,29,21,13,5, 
-    63,55,47,39,31,23,15,7 
-    }; 
-    // The expansion table
-    int expansion_table[48] = { 
-    32,1,2,3,4,5,4,5, 
-    6,7,8,9,8,9,10,11, 
-    12,13,12,13,14,15,16,17, 
-    16,17,18,19,20,21,20,21, 
-    22,23,24,25,24,25,26,27, 
-    28,29,28,29,30,31,32,1 
-    }; 
+	int initial_permutation[32] = { 
+	30, 26, 22, 18, 14, 10, 6, 2, 
+	32, 28, 24, 20, 16, 12, 8, 4, 
+	29, 25, 21, 17, 13, 9, 5, 1, 
+	31, 27, 23, 19, 15, 11, 7, 3
+	}; 
+	// The expansion table
+	int expansion_table[24] = { 
+	16, 1, 2, 3, 4, 5, 4, 5,
+	6, 7, 8, 9, 8, 9, 10, 11,
+	12, 13, 12, 13, 14, 15, 16, 1
+	}; 
     // The substitution boxes. The should contain values
-    // from 0 to 15 in any order.
-    int substition_boxes[8][4][16]=  
-    {{ 
-        14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7, 
-        0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8, 
-        4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0, 
-        15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13 
+
+//CAMBIO 2
+//las cajas de substitucion 
+	int substition_boxes[8][4][8]=  
+	{{ 
+        4,1,2,3,6,5,0,7, 
+        0,7,4,2,1,6,5,3, 
+        4,1,6,2,7,3,5,0, 
+        2,4,1,7,5,3,0,6 
     }, 
     { 
-        15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10, 
-        3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5, 
-        0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15, 
-        13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9 
+        1,6,3,4,7,2,0,5, 
+        3,4,7,2,0,1,6,5, 
+        0,7,4,1,5,6,3,2, 
+        1,3,4,2,6,7,0,5 
     }, 
     { 
-        10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8, 
-        13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1, 
-        13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7, 
-        1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12 
+        0,6,3,5,1,7,4,2, 
+        7,0,3,4,6,2,5,1, 
+        6,4,3,0,1,2,5,7, 
+        1,0,6,7,4,3,5,2 
     }, 
     { 
-        7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15, 
-        13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9, 
-        10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4, 
-        3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14 
+        7,3,0,6,1,2,5,4, 
+        5,6,0,3,4,7,2,1, 
+        6,0,7,1,3,5,2,4, 
+        3,0,6,1,4,5,7,2 
     }, 
     { 
-        2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9, 
-        14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6, 
-        4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14, 
-        11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3 
+        2,4,1,7,6,5,3,0, 
+        2,4,7,1,5,0,3,6, 
+        4,2,1,7,5,6,3,0, 
+        7,1,2,6,0,4,5,3 
     }, 
     { 
-        12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11, 
-        10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8, 
-        9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6, 
-        4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13 
+        1,2,6,0,3,4,7,5, 
+        4,2,7,5,6,1,0,3, 
+        5,2,3,7,0,4,1,6, 
+        4,3,2,5,1,7,6,0 
     }, 
     { 
-        4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1, 
-        13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6, 
-        1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2, 
-        6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12 
+        4,2,0,3,7,5,6,1, 
+        0,7,4,1,3,5,2,6, 
+        1,4,3,7,6,0,5,2, 
+        6,1,4,7,5,0,2,3 
     }, 
     { 
-        13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7, 
-        1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2, 
-        7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8, 
-        2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11 
+        2,4,6,1,3,5,0,7, 
+        1,3,7,4,5,6,0,2, 
+        7,4,1,2,0,6,3,5, 
+        2,1,7,4,0,3,5,6 
     }};
-	
-	// The permutation table
-    int permutation_tab[32] = { 
-    16,7,20,21,29,12,28,17, 
-    1,15,23,26,5,18,31,10, 
-    2,8,24,14,32,27,3,9,
-    19,13,30,6,22,11,4,25 
-    }; 
-    // The inverse permutation table
-    int inverse_permutation[64]= { 
-    40,8,48,16,56,24,64,32, 
-    39,7,47,15,55,23,63,31, 
-    38,6,46,14,54,22,62,30, 
-    37,5,45,13,53,21,61,29, 
-    36,4,44,12,52,20,60,28, 
-    35,3,43,11,51,19,59,27, 
-    34,2,42,10,50,18,58,26, 
-    33,1,41,9,49,17,57,25 
-    };
-	
-	string perm = ""; 
-    for(int i = 0; i < 64; i++){ 
-        perm += oldString[initial_permutation[i]-1]; 
-    }  
-    // 2. Dividing the result into two equal halves 
-    string left = perm.substr(0, 32); 
-    string right = perm.substr(32, 32);
-    // The plain text is encrypted 16 times  
-    for(int i=0; i<2; i++) { 
-        string right_expanded = ""; 
+
+// The permutation table
+	int permutation_tab[16] = { 
+		14, 10, 6, 2,
+		16, 12, 8, 4,
+		13, 9, 5, 1,
+		15, 11, 7, 3
+	}; 
+	// The inverse permutation table
+	int inverse_permutation[32]= { 
+	20, 4, 24, 8, 28, 12, 32, 16,
+	19, 3, 23, 7, 27, 11, 31, 15,
+	18, 2, 22, 6, 26, 10, 30, 14,
+	17, 1, 21, 5, 25, 9, 29, 13
+	};
+
+	string perm = "";
+	for(int i = 0; i < 32; i++){ 
+		perm += oldString[initial_permutation[i]-1]; 
+	}  
+	// 2. Dividing the result into two equal halves 
+	string left = perm.substr(0, 16); 
+	string right = perm.substr(16, 16);
+	// The plain text is encrypted 2 times  
+	for(int i=0; i<2; i++) { 
+    	string right_expanded = ""; 
         // 3.1. The right half of the plain text is expanded
         for(int i = 0; i < 48; i++) { 
             right_expanded += right[expansion_table[i]-1]; 
@@ -389,14 +381,14 @@ void *cypherText(void *argument){
         } 
         // 3.5. Another permutation is applied
         string perm2 =""; 
-        for(int i = 0; i < 32; i++){ 
+        for(int i = 0; i < 16; i++){ 
             perm2 += res[permutation_tab[i]-1]; 
         }
         // 3.6. The result is xored with the left half
         xored = Xor(perm2, left);
         // 3.7. The left and the right parts of the plain text are swapped 
         left = xored; 
-        if(i < 2){ 
+        if(i < 1){ 
             string temp = right;
             right = xored;
             left = temp;
@@ -406,7 +398,7 @@ void *cypherText(void *argument){
     string combined_text = left + right;   
     string ciphertext =""; 
     // The inverse of the initial permuttaion is applied
-    for(int i = 0; i < 64; i++){ 
+    for(int i = 0; i < 32; i++){ 
         ciphertext+= combined_text[inverse_permutation[i]-1]; 
     }
 	
